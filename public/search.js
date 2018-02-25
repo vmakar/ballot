7508a8ce2494afbeadda8219f6edee2c2813e4f7
vmakar <vmakar2@illinois.edu>
2018-02-25 06:18:28 -0600

    added function to list all upcoming elections in arrays

---
+++ public/search.js
@@ -0,0 +1,115 @@
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false);
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
+}
+
+var websiteString;
+websiteString = httpGet('https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyBNkHIUhVlCMVe5ljh68mIJLfk_m4TgFOk');
+
+parsedWebsite = JSON.parse(websiteString);
+arrayWebsite = Object.values(parsedWebsite);
+firstElement = arrayWebsite[1];
+firstElement[1].name;
+len = firstElement.length-1;
+var ids = new Array(len);
+var names = new Array(len);
+var days = new Array(len);
+var temp = new Array(len);
+var state = new Array(len);
+
+for (i = 1; i <= len; i++) { 
+    console.log(i);
+    element = firstElement[i];
+    ids[i-1] = element.id;
+    names[i-1] = element.name;
+    days[i-1] = element.electionDay;
+    temp[i-1] = element.ocdDivisionId;
+    var n = temp[i-1].indexOf("state:")+6;
+    console.log(n);
+    state[i-1] = temp[i-1].substring(n,n+2);   
+    
+}
+console.log(state)
+
+
+function abbrState(input, to){
+    
+    var states = [
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],        
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],        
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+    ];
+
+    if (to == 'abbr'){
+        input = input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+        for(i = 0; i < states.length; i++){
+            if(states[i][0] == input){
+                return(states[i][1]);
+            }
+        }    
+    } else if (to == 'name'){
+        input = input.toUpperCase();
+        for(i = 0; i < states.length; i++){
+            if(states[i][1] == input){
+                return(states[i][0]);
+            }
+        }    
+    }
+}
+console.log(state.length)
+for (j = 0; j < state.length; j++) 
+{ 
+    temp = state[j];
+    console.log(temp)
+    state[j] = abbrState(temp, "name")
+}
+
